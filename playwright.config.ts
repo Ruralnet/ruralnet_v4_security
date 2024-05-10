df3f5a8bf71ffc512c435aa8afea2f9d4e5cca95
mardev23-rnet <156924205+mardev23-rnet@users.noreply.github.com>
2024-05-10 15:59:16 +0800

    Update playwright.config.ts

--- playwright.config.ts
+++ playwright.config.ts
@@ -44,7 +44,7 @@ export default defineConfig({
 
     {
       name: 'webkit',
-      use: { ...devices['Desktop Safari'] },
+      use: { ...devices['Desktop Safari'], ignoreHTTPSErrors: true, },
     },
 
     /* Test against mobile viewports. */
